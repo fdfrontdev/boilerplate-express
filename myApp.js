22afeb7c0007f1f7a83ae8b05bd983c5c675c1e4
sdhmnjadilumrah <115298524+sdhmnjadilumrah@users.noreply.github.com>
2024-01-02 15:02:14 +0800

    Update myApp.js

--- myApp.js
+++ myApp.js
@@ -6,7 +6,7 @@ app.get('/', (req,res) =>{
     res.sendFile(__dirname + '/views/index.html')
 })
 
-express.static(__dirname + '/public',app.use());
+app.use(express.static(__dirname + '/public'));
 
 
 
