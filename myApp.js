538506782eb2a688dd0e00734295dcf57730644c
sdhmnjadilumrah <115298524+sdhmnjadilumrah@users.noreply.github.com>
2024-01-02 15:18:54 +0800

    update

--- myApp.js
+++ myApp.js
@@ -1,3 +1,4 @@
+require('dotenv').config()
 let express = require('express');
 let app = express();
 
@@ -11,7 +12,15 @@ app.get('/', (req, res) => {
 app.use("/public",express.static( __dirname + '/public'));
 
 app.get('/json',(req,res)=>{
-    res.json({"message":"Hello json"})
+
+    if(process.env.MESSAGE_STYLE == "uppercase"){
+        res.json({"message":"HELLO JSON"})
+    }
+    else{
+        res.json({"message":"Hello json"})
+    }
+
+    
 })
 
 
