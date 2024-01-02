159975b836ae6840e478795d17bcae475964aad7
sdhmnjadilumrah <115298524+sdhmnjadilumrah@users.noreply.github.com>
2024-01-02 15:11:43 +0800

    Update myApp.js

--- myApp.js
+++ myApp.js
@@ -1,12 +1,18 @@
 let express = require('express');
 let app = express();
 
+// return html to root path
 app.get('/', (req, res) => {
 
     res.sendFile(__dirname + '/views/index.html')
 })
 
+// mount the public folder
 app.use("/public",express.static( __dirname + '/public'));
+
+app.get('/json',(req,res)=>{
+    res.json({"message":"Hello json"})
+})
 
 
 
